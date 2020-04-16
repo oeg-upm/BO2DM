eb4e2d083f0016684204899daaa7db074734d237
Serge3006 <chavezferia.justinian@gmail.com>
2020-04-16 10:28:38 +0200

    first version of the web app converter working

--- source/api_flask.py
+++ source/api_flask.py
@@ -6,17 +6,36 @@ from converter import o2dm_conversion
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+domains_available = {
+    "occupancy_profile": {"prefix": "op", "name": "Occupancy Profile"},
+    "key_performance_indicator": {"prefix": "kpi", "name": "Key Performance Indicator"},
+    "weather_data": {"prefix": "weather", "name": "Weather Data"},
+    "sensor_data": {"prefix": "sd", "name": "Sensor Data"}
+}
+
+
 @app.route("/", methods=["GET"])
 def home():
-    return "<h1>BIMERR Ontology to Data Model (BO2DM)<h1>" \
-           "<p>Demo API for the ontology to data model" \
+    return "<h1>BIMERR Ontology to Data Model (BO2DM)</h1>" \
+           "<p>Demo API for the ontology to data model " \
            "converter to be used by BIMERR project.</p>"
 
-@app.route("/", methods=["GET"])
+@app.route("/parameters", methods=["GET"])
+def return_domains():
+
+    message = ""
+    for key, value in domains_available.items():
+        message = message + "<p><b>" + value["name"] + ":</b>" + "&ensp;" + key + "</p>"
+    return "<h1>Domains Available for Conversion</h1>" + message
+
+@app.route("/domains", methods=["GET"])
 def converter():
     query_parameters = request.args
-
-    ont_uri = query_parameters.get("uri")
-    data_model = o2dm_conversion(ont_uri)
+    domain = query_parameters.get("domain")
+    prefix = domains_available[domain]["prefix"]
+    ontology_path = "ontology/" + prefix + "_enriched.ttl"
+    data_model = o2dm_conversion(ontology_path)
 
     return jsonify(data_model)
+
+app.run()
